b892810aa2db9cc3b63a5dd44025b9ce174235f2
SteevoMcCool <96311500+SteevoMcCool@users.noreply.github.com>
2023-04-13 00:54:44 -0400

    Revert "s"
    This reverts commit 4f0b1b5d86f71325b7b489e55394bf331d0c3119.

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const fs = require("fs")
 const port = 6969
 server.use("/",(req,res)=>{
     var url = URL.parse(req.url);
-    console.log("Index path: "+ url.pathname)
-
     if (url.pathname=="/"){
         res.writeHead(200,{'Content-Type':'text/html'})
         url.pathname="/index.html"
     } else{
         res.writeHead(200,{'Content-Type':req.rawHeaders[13]})
+
     }
     fs.readFile("./Webpage" +url.pathname,function(error,data){
         if (error){
@@ -24,5 +23,55 @@ server.use("/",(req,res)=>{
     })
 })
 server.listen(port, function(error){
-    console.log(error|| "All good on port " + port + "\n http://localhost:6969")
+    console.log(error|| "All good on port " + port)
 })
+
+/*const http = require("http")
+const URL = require("url")
+const fs = require("fs")
+const webSocket = require("websocket")
+const port = 6969
+const server = http.createServer(function(req,res){
+    var url = URL.parse(req.url);
+    res.writeHead(200,{'Content-Type':'text/html'})
+    if (url.pathname=="/"){
+        url.pathname="/index.html"
+    } 
+    fs.readFile("./Webpage" +url.pathname,function(error,data){
+        if (error){
+        res.writeHead(404)
+        res.write("File not found: "+url.pathname)
+        }else{
+        res.write(data)
+        }
+        res.end()
+    })
+
+})
+server.listen(port, '127.0.0.1', function(error){
+    console.log(error|| "All good on port " + port)
+})
+
+
+
+
+// create the server
+
+let wsServer = new webSocket.server({
+
+    httpServer: server
+ 
+ });
+ 
+wsServer.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+    console.log(request)
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+
+        }
+    });
+    connection.on('close', function(connection) {
+    });
+ });
+ */
